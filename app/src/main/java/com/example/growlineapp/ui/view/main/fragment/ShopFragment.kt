79978577bdfb2e6344298539544b5f75c5004aba
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2021-12-16 19:42:10 +0900

    [FIX] 랭킹화면 상점으로 변경

--- app/src/main/java/com/example/growlineapp/ui/view/main/fragment/ShopFragment.kt
+++ app/src/main/java/com/example/growlineapp/ui/view/main/fragment/ShopFragment.kt
@@ -2,13 +2,12 @@ package com.example.growlineapp.ui.view.main.fragment
 
 import com.example.growlineapp.R
 import com.example.growlineapp.base.BaseFragment
-import com.example.growlineapp.databinding.FragmentRankBinding
+import com.example.growlineapp.databinding.FragmentShopBinding
 
-class RankFragment : BaseFragment<FragmentRankBinding>(R.layout.fragment_rank){
+class ShopFragment : BaseFragment<FragmentShopBinding>(R.layout.fragment_shop){
 
 
     override fun init() {
     }
-
 
 }
